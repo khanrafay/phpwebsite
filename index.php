796aaaa9243538918fff91def7f4c7a85da75ddb
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2024-10-23 14:06:32 +0300

    fixed htaccess

--- index.php
+++ index.php
@@ -3,17 +3,4 @@
 <?php
 require "functions.php";
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-// dd($uri);
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-    '/mission' => 'controllers/mission.php',
-];
-
-
-if (array_key_exists($uri, $routes)) {
-    require  $routes[$uri];
-}
+require "router.php";
