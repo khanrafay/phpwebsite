f5954e1488a9b3f02aa9c9343e98103f4af84cc2
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2025-01-17 14:29:13 +0300

    learned db connection and sql injection

--- index.php
+++ index.php
@@ -3,4 +3,24 @@
 <?php
 require "functions.php";
 
-require "router.php";
+//require "router.php";
+require "Database.php";
+
+//Connect databse and execute query
+
+$config = require 'config.php';
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+
+$query = "SELECT * FROM users where id = :id";
+
+
+$posts = $db->query($query, [':id' => $id])->fetchAll();
+
+
+dd($posts);
+// foreach ($posts as $post) {
+//     echo "<li>" . $post['title'] . "</li>";
+// }
